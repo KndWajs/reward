440af78e7c757a3c50c53fca5e66a59cd3020b0a
Konrad_Wajs <konrad.wajs@codelab.eu>
2022-11-07 21:44:13 +0100

    add exception handler for internal errors

--- src/main/java/tech/wajs/reward/exceptions/ErrorHandler.java
+++ src/main/java/tech/wajs/reward/exceptions/ErrorHandler.java
@@ -4,6 +4,8 @@
 
 package tech.wajs.reward.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,9 +13,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.List;
 
 @ControllerAdvice
+@Slf4j
 public class ErrorHandler {
     @ExceptionHandler(KnownException.class)
     public ResponseEntity handleKnownException(KnownException e) {
         return ResponseEntity.status(e.getResponseCode()).body(List.of(e.getMessage(), e.getErrorId().toString()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleKnownException(Exception e) {
+        KnownException knownException = new KnownException("Internal error.", HttpStatus.INTERNAL_SERVER_ERROR);
+        log.error(knownException.getErrorId().toString());
+        e.printStackTrace();
+        return ResponseEntity.status(knownException.getResponseCode())
+                             .body(List.of(knownException.getMessage(), knownException.getErrorId().toString()));
     }
 }
